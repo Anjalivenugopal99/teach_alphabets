a8aa96d2b9ee50246b545c236473566c3d63398d
Visakh Sakthidharan <anjali94venu@gmail.com>
2021-05-04 21:22:55 -0500

    added random number method saved

--- letters_script.js
+++ letters_script.js
@@ -31,27 +31,28 @@ function erase() {
     ctx.fillRect(0,0,280,280);
 }
 
-function getRandomNum(min, max){  
-    // generate and return a random number for the image to be displayed   
-    imgNo = Math.floor(Math.random() * (max - min + 1)) + min;  
-    return imageNameArray[imgNo];  
-}  
-
 function getAndAddImage(){
+    var imageNameArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     // 0 is first image and (preBuffer.length - 1) is last image of the array  
-    var randomLetter = getRandomNum(0, imageNameArray.length - 1); 
-    var img_url = '{% static "/data/pics/filename.png" %}'.replace(filename,randomLetter);
-    var newimg = document.createElement("newimg");
-    newimg.src = img_url;
-    
-    // remove the previous images  
-    var images = document.getElementsByTagName('letterImg');  
-    var l = images.length;  
-    for (var p = 0; p < l; p++) {  
-        images[0].parentNode.removeChild(images[0]);  
+    function getRandomNum(min, max){  
+        // generate and return a random number for the image to be displayed   
+        var imgNo = Math.floor(Math.random() * (max - min + 1)) + min;  
+        return imageNameArray[imgNo];  
     }  
-    // display the new random image    
-    document.body.appendChild(newimg); 
+    var randomLetter = getRandomNum(0, imageNameArray.length - 1);
+    alert(randomLetter);
+    // var img_url = '{% static "/data/pics/filename.png" %}'.replace(filename,randomLetter);
+    // var newimg = document.createElement("newimg");
+    // newimg.src = img_url;
+    
+    // // remove the previous images  
+    // var images = document.getElementsByTagName('letterImg');  
+    // var l = images.length;  
+    // for (var p = 0; p < l; p++) {  
+    //     images[0].parentNode.removeChild(images[0]);  
+    // }  
+    // // display the new random image    
+    // document.body.appendChild(newimg); 
 
 }
     
@@ -86,6 +87,7 @@ function init() {
     saveButton.addEventListener("click", save);
     clearButton = document.getElementById('cb');
     clearButton.addEventListener("click", erase);
+    getAndAddImage();
 }
 
 
